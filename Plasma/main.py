cdfb1c2a3f6ca04487ef748dde604a8bfd5c7a73
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-02-21 17:56:08 +0700

    Update main.py

--- Plasma/main.py
+++ Plasma/main.py
@@ -1,3 +1,1 @@
-# Do not waste my time by asking nonsense questions, just notify me error in my scripts or request bot in BOT TOPIC
-
-# Or even donation, LOL
+print("STOP UPDATING THIS SCRIPT, PLEASE CHOOSE ANOTHER ONE!")
